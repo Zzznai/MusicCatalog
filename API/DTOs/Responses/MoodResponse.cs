af562353512ef205a60da3f890a373c453b7099a
Zzznai <naimabazabaz@gmail.com>
2026-02-10 21:50:11 +0200

    Modify MoodResponse

--- API/DTOs/Responses/MoodResponse.cs
+++ API/DTOs/Responses/MoodResponse.cs
@@ -6,12 +6,22 @@ public class MoodResponse
 {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
-    public ICollection<Album> Albums { get; set; } = [];
+    public List<MoodAlbumResponse> Albums { get; set; } = [];
 
     public static MoodResponse FromEntity(Mood mood) => new()
     {
         Id = mood.Id,
         Name = mood.Name,
-        Albums = mood.Albums
+        Albums = mood.Albums.Select(a => new MoodAlbumResponse
+        {
+            Id = a.Id,
+            Name = a.Name
+        }).ToList()
     };
+}
+
+public class MoodAlbumResponse
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
 }
